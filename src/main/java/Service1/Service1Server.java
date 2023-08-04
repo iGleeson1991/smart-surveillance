cd06e90c9ecfa43225432cc90de475ad78496238
igleeson <ngleeson1991@gmail.com>
2023-08-04 20:59:52 +0100

    Coded server classes. Created templates for remote methods.

--- src/main/java/Service1/Service1Server.java
+++ src/main/java/Service1/Service1Server.java
@@ -1,12 +1,117 @@
 package Service1;
 
+import Service1.Service1Grpc.Service1ImplBase;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
 import javax.jmdns.JmDNS;
 import javax.jmdns.ServiceInfo;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.InetAddress;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
 
-import Service1.Service1Grpc.Service1ImplBase;
-
-public class Service1Server extends Service1ImplBase{
+public class Service1Server extends Service1ImplBase {
     public static void main(String[] args) {
+        //Creates a new "Service1Server" object called "service1Server"
+        Service1Server service1Server = new Service1Server();
+        String propertiesFileName = "Service1.properties";
+        //Creates a new "Properties" object
+        Properties properties = service1Server.getProperties(propertiesFileName);
+        service1Server.registerService(properties);
+        int port = Integer.parseInt(properties.getProperty("service_port"));
+
+        try {
+            //Creates a "Server" object called "server1" and uses the "ServerBuilder" class to build and start a new server for the designated port and service registered to "service1Server"
+            Server server1 = ServerBuilder.forPort(port).addService(service1Server).build().start();
+            //Prints the new server's info to the console
+            System.out.println("\nServer 1 Started: Door Controller. Listening on Port " + port);
+
+            //Waits for the server to terminate
+            server1.awaitTermination();
+
+        //Catches any IO or Interrupted Exceptions and prints them to the console
+        } catch (IOException | InterruptedException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    //Private Server Methods
+
+    //Creates a private method called "getProperties" that returns "Properties"
+    //Reads the data from the properties file using "InputStream" and prints them to the console
+    private Properties getProperties(String fileName) {
+        Properties properties = null; //Empty "Properties" object called "properties"
+
+        //Attempts to read data from the properties file using an "InputStream" called "input"
+        try (InputStream input = Files.newInputStream(Paths.get("Smart Surveillance/src/main/resources/",fileName))){
+            properties = new Properties(); //Makes "properties" equal to an empty property list
+            properties.load(input); //Loads the properties files from "input"
+            //Prints the data from the properties file
+            System.out.println("\n---- " + properties.getProperty("service_name") + " Properties ----");
+            System.out.println("Service Type: " + properties.getProperty("service_type"));
+            System.out.println("Service Name: " + properties.getProperty("service_name"));
+            System.out.println("Service Description: " + properties.getProperty("service_desc"));
+            System.out.println("Service Port: " + properties.getProperty("service_port"));
+
+        //Catches any IO Exceptions and prints them to the console
+        } catch (IOException ioe) {
+            System.out.println(ioe.getMessage());
+            ioe.printStackTrace();
+        }
+        return properties;
+    }
+
+    //Creates a private method to register the jmDNS service
+    //Takes in the data from the properties file stored in "properties and uses it to register the jmDNS service
+    private void registerService(Properties properties) {
+        //Attempts to register the jmDNS service
+        try {
+            //Creates a "JmDNS" object called "jmdns" and sets it to the IP address of the local host
+            JmDNS jmdns = JmDNS.create(InetAddress.getLocalHost());
+
+            //Stores the data from the properties file in appropriate variables
+            String service_type = properties.getProperty("service_type");
+            String service_name = properties.getProperty("service_name");
+            int service_port = Integer.parseInt(properties.getProperty("service_port"));
+            String service_desc = properties.getProperty("service_desc");
+
+            //Creates a "ServiceInfo" object called "serviceInfo" and sets it equal to a new service created with the data from the properties file
+            ServiceInfo serviceInfo = ServiceInfo.create(service_type, service_name, service_port, service_desc);
+            jmdns.registerService(serviceInfo);
+
+            //Prints a message to the console with the details of the service being registered
+            System.out.println("\nRegistering Service:\nService Type: " + service_type + "\nService Name: " + service_name + "\nService Description: " + service_desc + "\nService Port: " + service_port);
+
+            //Pause instruction
+            Thread.sleep(500);
+
+        //Catches any IO or Interrupted Exceptions and prints them to the console
+        } catch (IOException | InterruptedException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    //Remote Methods
+
+    public void scanSecurityBadge() {
+
+    }
+
+    public void securityCodeEntry() {
+
+    }
+
+    public void intercomCall() {
+
+    }
+
+    public void oneWayCommunication() {
 
     }
 }
+
--- src/main/java/Service2/Service2Server.java
+++ src/main/java/Service2/Service2Server.java
@@ -1,7 +1,112 @@
 package Service2;
 
-public class Service2Server {
+import Service2.Service2Grpc.Service2ImplBase;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import javax.jmdns.JmDNS;
+import javax.jmdns.ServiceInfo;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.InetAddress;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+public class Service2Server extends Service2ImplBase {
     public static void main(String[] args) {
+        //Creates a new "Service2Server" object called "service2Server"
+        Service2Server service2Server = new Service2Server();
+        String propertiesFileName = "Service2.properties";
+        //Creates a new "Properties" object
+        Properties properties = service2Server.getProperties(propertiesFileName);
+        service2Server.registerService(properties);
+        int port = Integer.parseInt(properties.getProperty("service_port"));
+
+        try {
+            //Creates a "Server" object called "server1" and uses the "ServerBuilder" class to build and start a new server for the designated port and service registered to "service1Server"
+            Server server2 = ServerBuilder.forPort(port).addService(service2Server).build().start();
+            //Prints the new server's info to the console
+            System.out.println("Server 2 Started: Door Controller. Listening on Port " + port);
+
+            //Waits for the server to terminate
+            server2.awaitTermination();
+
+            //Catches any IO or Interrupted Exceptions and prints them to the console
+        } catch (IOException | InterruptedException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    //Private Server Methods
+
+    //Creates a private method called "getProperties" that returns "Properties"
+    //Reads the data from the properties file using "InputStream" and prints them to the console
+    private Properties getProperties(String fileName) {
+        Properties properties = null; //Empty "Properties" object called "properties"
+
+        //Attempts to read data from the properties file using an "InputStream" called "input"
+        try (InputStream input = Files.newInputStream(Paths.get("Smart Surveillance/src/main/resources/",fileName))){
+            properties = new Properties(); //Makes "properties" equal to an empty property list
+            properties.load(input); //Loads the properties files from "input"
+            //Prints the data from the properties file
+            System.out.println("---- " + properties.getProperty("service_name") + " Properties ----");
+            System.out.println("Service Type: " + properties.getProperty("service_type"));
+            System.out.println("Service Name: " + properties.getProperty("service_name"));
+            System.out.println("Service Description: " + properties.getProperty("service_desc"));
+            System.out.println("Service Port: " + properties.getProperty("service_port"));
+
+            //Catches any IO Exceptions and prints them to the console
+        } catch (IOException ioe) {
+            System.out.println(ioe.getMessage());
+            ioe.printStackTrace();
+        }
+        return properties;
+    }
+
+    //Creates a private method to register the jmDNS service
+    //Takes in the data from the properties file stored in "properties and uses it to register the jmDNS service
+    private void registerService(Properties properties) {
+        //Attempts to register the jmDNS service
+        try {
+            //Creates a "JmDNS" object called "jmdns" and sets it to the IP address of the local host
+            JmDNS jmdns = JmDNS.create(InetAddress.getLocalHost());
+
+            //Stores the data from the properties file in appropriate variables
+            String service_type = properties.getProperty("service_type");
+            String service_name = properties.getProperty("service_name");
+            int service_port = Integer.parseInt(properties.getProperty("service_port"));
+            String service_desc = properties.getProperty("service_desc");
+
+            //Creates a "ServiceInfo" object called "serviceInfo" and sets it equal to a new service created with the data from the properties file
+            ServiceInfo serviceInfo = ServiceInfo.create(service_type, service_name, service_port, service_desc);
+            jmdns.registerService(serviceInfo);
+
+            //Prints a message to the console with the details of the service being registered
+            System.out.println("Registering Service:\nService Type: " + service_type + "\nService Name: " + service_name + "\nService Description: " + service_desc + "\nService Port: " + service_port);
+
+            //Pause instruction
+            Thread.sleep(500);
+
+            //Catches any IO or Interrupted Exceptions and prints them to the console
+        } catch (IOException | InterruptedException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    //Remote Methods
+
+    public void cameraAdjustment() {
+
+    }
+
+    public void motionDetected() {
+
+    }
+
+    public void cameraAutomation() {
 
     }
 }
